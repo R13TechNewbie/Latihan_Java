ceba741ddd5a0c6eebf251371c3a773f6c4576a9
Ramadhan Hidayat <ramadhanhidayat13@gmail.com>
2018-09-12 23:27:25 +0000

    adding sets_sololearn.java

---
+++ JAVA/src/main/java/com/r13technewbie/java/SoloLearn/sets_sololearn.java
@@ -0,0 +1,18 @@
+package com.r13technewbie.java.SoloLearn;
+
+import java.util.HashSet;
+
+class sets_sololearn {
+    
+    public static void main (String[] args) {
+        
+        HashSet<String> unik = new HashSet<String>();
+        unik.add("ayam");
+        unik.add("sapi");
+        unik.add("kambing");
+        unik.add("ayam");
+        System.out.println(unik);
+        
+    }
+    
+}
